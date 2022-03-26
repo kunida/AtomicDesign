c1f602d6edc0f625043d788eda8911fd493b8c08
kunida <67056950+kunida@users.noreply.github.com>
2022-03-26 10:40:25 +0900

    molecule,organism

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "くったん",
+  image: "https://source.unsplash.com/hgHyIGi9sY4",
+  email: "12345@example.com",
+  phone: "0000-00-0000",
+  company: {
+    name: "React株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
